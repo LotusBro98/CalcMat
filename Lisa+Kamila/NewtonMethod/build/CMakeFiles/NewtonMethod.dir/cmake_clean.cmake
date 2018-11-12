c8080eda89426fafdcca6b3d16c7470f23d95e3a
LotusBro98 <LotusBro98@yandex.ru>
2018-11-13 02:59:06 +0300

    Made Newton method in c++

---
+++ Lisa+Kamila/NewtonMethod/build/CMakeFiles/NewtonMethod.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewtonMethod.dir/main.cpp.o"
+  "NewtonMethod.pdb"
+  "NewtonMethod"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewtonMethod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
